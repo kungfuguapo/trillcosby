f907c6e588cb3c8bdf84e25b085dbda5f8c1c730
Flosstradamus <flosstradamus@turntupinthe.club>
2015-03-20 09:28:42 -0500

    Serialized Array, Update v.1

--- testfile.php
+++ testfile.php
@@ -2,6 +2,11 @@
 /* Data serialization test */
 
 $data = array ('apple','banana','orange');
-echo serialize ( $data );
+$serialized_data = serialize ( $data );
+echo ( $serialized_data );
+$unserialized_data = unserialize ( $serialized_data );
+echo $unserialized_data[0];
+echo $unserialized_data[1];
+echo $unserialized_data[2];
 
 ?>
